2387bc691f59fa51357e13daaf7b09825b2771d0
JeeJ <gustavogomescoding@gmail.com>
2025-05-23 10:32:08 -0300

    resolvi

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-Inputmask({"mask": "(99) 99999-9999"}).mask(document.getElementById("fone"));
-Inputmask({"mask": "999.999.999-99"}).mask(document.getElementById("cpf"))
+Inputmask({ mask: "(99) 99999-9999" }).mask(document.getElementById("fone"));
+Inputmask({ mask: "999.999.999-99" }).mask(document.getElementById("cpf"));
 
-let funcionarios = []
+let funcionarios = [];
+let bsModal;
 
-document.querySelector("#btn-cadastrar").addEventListener("click", ()=> {
-    let nome = document.querySelector("#nome").value
-    let idade = document.querySelector("#idade").value
-    let cidade = document.querySelector("#cidade").value
-    let fone = document.querySelector("#fone").value
-    let cpf = document.querySelector("#cpf").value
+document.querySelector("#btn-adicionar").addEventListener("click", () => {
+  instanciaModal = bootstrap.Modal.getInstance(
+    document.getElementById("exampleModal")
+  );
+});
 
-    if (nome == "" || idade == "" || cidade == "" || fone == "" || cpf == ""){
-        alert("Algum dado ainda está faltando!")
-        return
-    }
+document.querySelector("#btn-cadastrar").addEventListener("click", () => {
+  let nome = document.querySelector("#nome").value;
+  let idade = document.querySelector("#idade").value;
+  let cidade = document.querySelector("#cidade").value;
+  let fone = document.querySelector("#fone").value;
+  let cpf = document.querySelector("#cpf").value;
 
-    let pessoa = {
-        "nome": nome,
-        "idade": idade,
-        "cidade": cidade,
-        "fone": fone,
-        "cpf": cpf
-    }
+  if (nome == "" || idade == "" || cidade == "" || fone == "" || cpf == "") {
+    alert("Algum dado ainda está faltando!");
+    return;
+  }
 
-    funcionarios.push(pessoa)
+  let pessoa = {
+    nome: nome,
+    idade: idade,
+    cidade: cidade,
+    fone: fone,
+    cpf: cpf,
+  };
 
-    console.log(funcionarios)
-})
+  funcionarios.push(pessoa);
+
+  instanciaModal.hide();
+
+  console.log(funcionarios);
+});
