5342f72bf9e6cae5ce96a5439224ebe877d8b929
JeeJ <gustavogomescoding@gmail.com>
2025-05-27 11:01:56 -0300

    .

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ Inputmask({ mask: "999.999.999-99" }).mask(document.querySelector("#cpf"));
 let funcionarios = [];
 let instanciaModal;
 
-console.log(document.getElementsByClassName("container")[0]);
+let indexEditado;
+let linhaEditada;
 
 const tabela = document.querySelector("#corpo-tabela");
 
@@ -14,133 +15,25 @@ document.querySelector("#btn-adicionar").addEventListener("click", () => {
   );
 });
 
-tabela.addEventListener("click", (elemClicado) => {
-  let botaoOlhar = elemClicado.target.closest(".btn-olhar");
-  let botaoExcluir = elemClicado.target.closest(".btn-excluir");
-  let botaoEditar = elemClicado.target.closest(".btn-editar");
-
-  if (botaoOlhar) {
-    //A cada vez que eu clico, ele refaz a contagem dos botões e linhas disponíveis, o que me ajuda a pegar o index para o array FUNCIONÁRIOS
-
-    let index = Array.from(document.querySelectorAll(".btn-olhar")).indexOf(
-      botaoOlhar
-    );
-
-    let pessoa = funcionarios[index];
-
-    document.querySelector("#nomeOlhar").value = pessoa.nome;
-    document.querySelector("#idadeOlhar").value = pessoa.idade;
-    document.querySelector("#cidadeOlhar").value = pessoa.cidade;
-    document.querySelector("#foneOlhar").value = pessoa.fone;
-    document.querySelector("#cpfOlhar").value = pessoa.cpf;
-  }
-
-  if (botaoExcluir) {
-    let linhaBotao = botaoExcluir.closest("tr");
-    let listaLinhas = Array.from(tabela.querySelectorAll("tr"));
-    let index = listaLinhas.indexOf(linhaBotao);
-
-    funcionarios.splice(index, 1);
-    linhaBotao.remove(); // Eu removi dai completamente do DOM lá
-  }
-
-  if (botaoEditar) {
-    instanciaModal = bootstrap.Modal.getInstance(
-      document.getElementById("modalEditar")
-    );
-
-    let index = Array.from(tabela.querySelectorAll(".btn-editar")).indexOf(
-      botaoEditar
-    );
-
-    console.log(funcionarios[index])
-
-    document.querySelector("#nomeEditar").value = funcionarios[index].nome;
-    document.querySelector("#idadeEditar").value = funcionarios[index].idade;
-    document.querySelector("#cidadeEditar").value = funcionarios[index].cidade;
-    document.querySelector("#foneEditar").value = funcionarios[index].fone;
-    document.querySelector("#cpfEditar").value = funcionarios[index].cpf;
-
-    let indexEditado = index
-
-    document.querySelector("#btn-confirmarEdicao").addEventListener("click", () => {
-      let nome = document.querySelector("#nomeEditar").value;
-      let idade = document.querySelector("#idadeEditar").value;
-      let cidade = document.querySelector("#cidadeEditar").value;
-      let fone = document.querySelector("#foneEditar").value;
-      let cpf = document.querySelector("#cpfEditar").value;
-    
-      if (nome == "" || idade == "" || cidade == "" || fone == "" || cpf == "") {
-        alert("Algum dado ainda está faltando!");
-        return;
-      }
-    
-      let pessoa = {
-        nome: nome,
-        idade: idade,
-        cidade: cidade,
-        fone: fone,
-        cpf: cpf,
-      };
-    
-      funcionarios[indexEditado] = pessoa;
-
-      console.log(funcionarios[index])
-
-      let linhaAtual = botaoEditar.closest("tr")
-
-      linhaAtual.innerHTML = `
-        <td>${funcionarios[index].nome}</td>
-        <td>${funcionarios[index].idade}</td>
-        <td>${funcionarios[index].cidade}</td>
-        <td>${funcionarios[index].fone}</td>
-        <td>${funcionarios[index].cpf}</td>
-        <td>
-          <button class="btn btn-success btn-olhar" data-bs-toggle="modal" data-bs-target="#modalOlhar">
-            <i class="bi bi-eye"></i>
-          </button>
-          <button class="btn btn-primary btn-editar" data-bs-toggle="modal" data-bs-target="#modalEditar">
-            <i class="bi bi-pencil-square"></i>
-          </button>
-          <button class="btn btn-danger btn-excluir">
-            <i class="bi bi-trash"></i>
-          </button>
-        </td>
-      `
-      instanciaModal.hide();
-    });
-  }
-});
-
 document.querySelector("#btn-cadastrar").addEventListener("click", () => {
-  let nome = document.querySelector("#nome").value;
-  let idade = document.querySelector("#idade").value;
-  let cidade = document.querySelector("#cidade").value;
-  let fone = document.querySelector("#fone").value;
-  let cpf = document.querySelector("#cpf").value;
+  let result = coletaDados("nome", "idade", "cidade", "fone", "cpf");
 
-  if (nome == "" || idade == "" || cidade == "" || fone == "" || cpf == "") {
+  if (result == -1) {
     alert("Algum dado ainda está faltando!");
     return;
   }
 
-  let pessoa = {
-    nome: nome,
-    idade: idade,
-    cidade: cidade,
-    fone: fone,
-    cpf: cpf,
-  };
+  let pessoa = result;
 
   funcionarios.push(pessoa);
 
   tabela.innerHTML += `
     <tr>
-      <td>${nome}</td>
-      <td>${idade}</td>
-      <td>${cidade}</td>
-      <td>${fone}</td>
-      <td>${cpf}</td>
+      <td>${pessoa.nome}</td>
+      <td>${pessoa.idade}</td>
+      <td>${pessoa.cidade}</td>
+      <td>${pessoa.fone}</td>
+      <td>${pessoa.cpf}</td>
       <td>
         <button class="btn btn-success btn-olhar" data-bs-toggle="modal" data-bs-target="#modalOlhar">
           <i class="bi bi-eye"></i>
@@ -164,4 +57,113 @@ document.querySelector("#btn-cadastrar").addEventListener("click", () => {
   instanciaModal.hide();
 });
 
+tabela.addEventListener("click", (elemClicado) => {
+  let botaoOlhar = elemClicado.target.closest(".btn-olhar");
+  let botaoExcluir = elemClicado.target.closest(".btn-excluir");
+  let botaoEditar = elemClicado.target.closest(".btn-editar");
 
+  if (botaoOlhar) {
+    let index = Array.from(document.querySelectorAll(".btn-olhar")).indexOf(
+      botaoOlhar
+    );
+
+    let pessoa = funcionarios[index];
+
+    document.querySelector("#nomeOlhar").value = pessoa.nome;
+    document.querySelector("#idadeOlhar").value = pessoa.idade;
+    document.querySelector("#cidadeOlhar").value = pessoa.cidade;
+    document.querySelector("#foneOlhar").value = pessoa.fone;
+    document.querySelector("#cpfOlhar").value = pessoa.cpf;
+  }
+
+  if (botaoExcluir) {
+    let linhaBotao = botaoExcluir.closest("tr");
+    let listaLinhas = Array.from(tabela.querySelectorAll("tr"));
+    let index = listaLinhas.indexOf(linhaBotao);
+
+    funcionarios.splice(index, 1);
+    linhaBotao.remove();
+  }
+
+  if (botaoEditar) {
+    instanciaModal = bootstrap.Modal.getInstance(
+      document.getElementById("modalEditar")
+    );
+
+    indexEditado = Array.from(tabela.querySelectorAll(".btn-editar")).indexOf(
+      botaoEditar
+    );
+
+    document.querySelector("#nomeEditar").value =
+      funcionarios[indexEditado].nome;
+    document.querySelector("#idadeEditar").value =
+      funcionarios[indexEditado].idade;
+    document.querySelector("#cidadeEditar").value =
+      funcionarios[indexEditado].cidade;
+    document.querySelector("#foneEditar").value =
+      funcionarios[indexEditado].fone;
+    document.querySelector("#cpfEditar").value = funcionarios[indexEditado].cpf;
+
+    linhaEditada = botaoEditar.closest("tr");
+  }
+});
+
+document.querySelector("#btn-alterar").addEventListener("click", () => {
+  let result = coletaDados(
+    "nomeEditar",
+    "idadeEditar",
+    "cidadeEditar",
+    "foneEditar",
+    "cpfEditar"
+  );
+
+  if (result == -1) {
+    alert("Algum dado ainda está faltando!");
+    return;
+  }
+
+  funcionarios[indexEditado] = result;
+
+  linhaEditada.innerHTML = `
+    <td>${funcionarios[indexEditado].nome}</td>
+    <td>${funcionarios[indexEditado].idade}</td>
+    <td>${funcionarios[indexEditado].cidade}</td>
+    <td>${funcionarios[indexEditado].fone}</td>
+    <td>${funcionarios[indexEditado].cpf}</td>
+    <td>
+      <button class="btn btn-success btn-olhar" data-bs-toggle="modal" data-bs-target="#modalOlhar">
+        <i class="bi bi-eye"></i>
+      </button>
+      <button class="btn btn-primary btn-editar" data-bs-toggle="modal" data-bs-target="#modalEditar">
+        <i class="bi bi-pencil-square"></i>
+      </button>
+      <button class="btn btn-danger btn-excluir">
+        <i class="bi bi-trash"></i>
+      </button>
+    </td>
+  `;
+
+  instanciaModal.hide();
+});
+
+function coletaDados(nomeID, idadeID, cidadeID, foneID, cpfID) {
+  let nome = document.querySelector(`#${nomeID}`).value;
+  let idade = document.querySelector(`#${idadeID}`).value;
+  let cidade = document.querySelector(`#${cidadeID}`).value;
+  let fone = document.querySelector(`#${foneID}`).value;
+  let cpf = document.querySelector(`#${cpfID}`).value;
+
+  if (nome == "" || idade == "" || cidade == "" || fone == "" || cpf == "") {
+    return -1;
+  }
+
+  let pessoa = {
+    nome: nome,
+    idade: idade,
+    cidade: cidade,
+    fone: fone,
+    cpf: cpf,
+  };
+
+  return pessoa;
+}
